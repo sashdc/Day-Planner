24229be594443c177fedf6bab1a8a3ef6b646b84
saharsh <saharshdc@gmail.com>
2022-09-24 16:29:55 -0400

    fixed local storage and added notification

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,41 +2,49 @@ let currentDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDate);
 let saveBtn = $('.saveBtn');
 
+// colour codes time blocks so they indivcate if they are past/present/future
 function colorCode(){
     let currentTime = moment().format('HH');
     console.log(currentTime);
     let timeSlot = $('.time-block');
-    console.log(timeSlot);
-     console.log(timeSlot[0])
-   for (i=0; i<13; i++){
-    let milTime = timeSlot[i].getAttribute('id');
-    if (parseInt(currentTime)===parseInt(milTime)){
+   for (i=0; i<9; i++){
+    if (parseInt(currentTime)===(i+9)){
         timeSlot[i].classList.add("present");
-        timeSlot[i].classList.remove("past");
-        timeSlot[i].classList.remove("future");
         }
-    else if (parseInt(currentTime)>parseInt(milTime)){
+    else if (parseInt(currentTime)>(i+9)){
         timeSlot[i].classList.add("past");
-        timeSlot[i].classList.remove("present");
-        timeSlot[i].classList.remove("future");
             }
-    else if (parseInt(currentTime)<parseInt(milTime)){
+    else if (parseInt(currentTime)<(i+9)){
         timeSlot[i].classList.add("future");
-        timeSlot[i].classList.remove("past");
-        timeSlot[i].classList.remove("present");
                 }
 };
 }
 
-// function saveEntry(){
-//     console.log('clicky clicky')
-
-// }
-
+// calls the function of colour coding every second so that it is updated as time passes
 setInterval(colorCode(),1000);
 
-// saveBtn.on('click', function(){
-//     let eventId = $(this).attr('id');
-//     let eventText = $(this).parent().siblings().children('.time-block').val();
-//     localStorage.setItem(eventId, eventText);
-//   ) 
+
+
+
+$(".saveBtn").on("click", function(){
+    let saveSlot = $(this).parent().children().eq(1).attr("id");
+    let saveItem = $(this).parent().children().eq(1).val();
+    localStorage.setItem(saveSlot, saveItem);
+
+    let addNot = '<h5>Your Task has been added to localstorage ✅</h5>';
+    // addNot.addClass('justify-content-center')
+    $('.container').prepend(addNot);
+    // setTimeout(addNot.hide(),2000)
+});
+
+// function init() {
+//     let saveSlot = $('.hour')
+//     let saveItem = $('.time-block')
+//     let value = localStorage.getItem(saveSlot, saveItem);
+//     if (value) {
+//         $(`#${key}`).text(value);
+//     }
+// }
+
+
+// init();
